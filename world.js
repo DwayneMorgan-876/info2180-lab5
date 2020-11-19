c316d3aeaaa5d43236377c75eb3f2ff8a3bab028
Dwayne Morgan <dwaynerowayne@gmail.com>
2020-11-19 18:16:26 -0500

    Updating Files

--- world.js
+++ world.js
@@ -1,35 +1,35 @@
-window.onload=function page(){
+function page(){
 
-  document.getElementsByClassName("lookup")[0].addEventListener("click", function(){
-    
-    var inputVal = document.getElementById("country").value;
+  document.getElementById("lookup").addEventListener("click", function(){      
+  var inputVal = document.getElementById("country").value;
+  var xhr = new XMLHttpRequest();
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200){
-      
-      document.getElementById("result").innerHTML = xhr.responseText;
+  xhr.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+        
+        document.getElementById("result").innerHTML = xhr.responseText;
 
+      }
     }
-  }
-xhr.open("GET", "world.php?country="+inputVal);
-xhr.send();
-})
+    xhr.open("GET", "world.php?country="+inputVal);
+    xhr.send();
+  });
+
+  document.getElementById("lookup-cities").addEventListener("click", function(){      
+  var inputVal = document.getElementById("country").value;
+  var xhr = new XMLHttpRequest();
 
 
-// document.getElementsByClassName("lookup-cities")[0].addEventListener("click", function(){
-    
-//     var inputVal = document.getElementById("country").value;
+  xhr.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+        
+        document.getElementById("result").innerHTML = xhr.responseText;
 
-// var xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function(){
-//     if (this.readyState == 4 && this.status == 200){
-      
-//       document.getElementById("result").innerHTML = xhr.responseText;
+      }
+    }
+  xhr.open("GET", "world.php?country=" +inputVal+ "&context=lookup-cities");
+  xhr.send();
+  });
 
-//     }
-//   }
-// xhr.open("GET", " world.php?city="+inputVal);
-// xhr.send();
-// })
 }
+window.onload=page;
--- world.php
+++ world.php
@@ -1,37 +1,78 @@
+<style>
+	table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+	}
+tr:nth-child(even){
+		background-color: white;
+	}
+
+tr:nth-child(odd){
+		background-color: #f7fafc;
+	}
+</style>
+
 <?php
 $host = getenv('IP');
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = (isset($_GET['country']) ? $_GET['country']:null);
-$cities = (isset($_GET['cities']) ? $_GET['cities']:null);
-
-// $country1 = filter_var($_GET['country'], FILTER_SANITIZE_STRING);
+$context = (isset($_GET['context']) ? $_GET['context']:null);
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
+
+// $stmt1= $conn->query("SELECT c.name as city, c.district, c.population FROM cities c JOIN countries cs ON c.country_code=cs.code WHERE cs.name like '%$country%'");
+
+// $results = $stmt1->fetchAll(PDO::FETCH_ASSOC);
 
 
+if (isset($country)==true && isset($GET_['context'])==false){
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	echo "<table>
+	 <tr>
+	    <th>Name</th>
+	    <th>Continent</th>
+	    <th>Independence</th>
+	    <th>Head of State</th>
+	 </tr>";
+	  
+	foreach ($results as $row){
+
+		 echo "<tr>";
+		 echo 	"<td>".$row['name']."</td>";
+		 echo  	"<td>".$row['continent']."</td>";
+		 echo  	"<td>".$row['independence_year']."</td>";
+		 echo  	"<td>".$row['head_of_state']."</td>";
+		 echo "</tr>";
+		 }  
+
+		echo "</table>";
+
+}elseif (isset($country)==true && isset($GET_['context'])==true) {
+	$stmt= $conn->query("SELECT c.name as city, c.district, c.population FROM cities c INNER JOIN countries cs ON c.country_code=cs.code WHERE cs.name LIKE '%$country%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
+	echo "<table>
+	<tr>
+		<th>Name</th>
+		<th>District</th>
+		<th>Population</th>
+	</tr>";
 
-<table border="5">
- <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of State</th>
- </tr>
-  
-<?php foreach ($results as $row): ?>
+	foreach ($results as $row){
+		// echo "<tr><td>" .$row['city']. "</td><td>".$row['district']."</td><td>".$row['population']."</td></tr>";
+		 echo "<tr>";
+		 echo 	"<td>".$row['city']."</td>";
+		 echo  	"<td>".$row['district']."</td>";
+		 echo  	"<td>".$row['population']."</td>";
+		 echo "</tr>";
+	}
+	echo "</table";
+// }else {
+// 	echo "0 Results Found";
+// }
+}
 
- <tr><td><?= $row['name']?></td>
-  	<td><?= $row['continent']?></td>
-  	<td><?= $row['independence_year']?></td>
-  	<td><?= $row['head_of_state']?></td>
- </tr>
-  
-  
-<?php endforeach; ?>
-</table>
